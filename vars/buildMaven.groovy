77676dec3905271fae5db5fc3946aef20284feca
chaitrae1688 <chaitrae1688@gmail.com>
2026-08-12 00:19:26 +0530

    code

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -3,9 +3,9 @@ def call(String golas = 'clean install' boolean skipTests = true) {
     echo "Starting maven building process........"
 
     String mvnCommand = "mvn ${goals}"
-    if (skipTests) {
-        mvnCommand  += " -DskipTests"
-    }
+    // if (skipTests) {
+    //     mvnCommand  += " -DskipTests"
+    // }
     
     sh mvnCommand
 
